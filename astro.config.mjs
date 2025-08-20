b771f62dbd7d067af8bcfcad23ac2061413411e5
Xaver Wenhart <git@xavwe.dev>
2025-08-20 23:46:51 +0200

    style: format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.xavwe.dev'
+  site: "https://www.xavwe.dev",
 });
