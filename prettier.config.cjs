0becfc1a5acd8ca8b13fcc16b8da98e90a8b15c6
Xaver Wenhart <git@xavwe.dev>
2025-08-20 23:41:21 +0200

    chore: add formatter

---
+++ prettier.config.cjs
@@ -0,0 +1,23 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+module.exports = config;
