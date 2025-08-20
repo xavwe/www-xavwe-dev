49145ede1f883e2c7a59f69579848c25511d887b
Xaver Wenhart <git@xavwe.dev>
2025-08-20 21:03:37 +0200

    chore: add dev-shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A astro project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells."x86_64-linux".default = nixpkgs.legacyPackages."x86_64-linux".mkShell {
+        packages = with nixpkgs.legacyPackages."x86_64-linux"; [
+          nodejs
+          nixd
+          nodePackages.pnpm
+          just
+          node2nix
+        ];
+      };
+  };
+}
