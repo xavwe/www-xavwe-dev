da7c02eb372e0eef4a922e0093d0aa67f916833e
Xaver Wenhart <git@xavwe.dev>
2025-08-21 09:17:31 +0200

    chore: add nix formatter

--- flake.nix
+++ flake.nix
@@ -5,14 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    formatter."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
     devShells."x86_64-linux".default = nixpkgs.legacyPackages."x86_64-linux".mkShell {
-        packages = with nixpkgs.legacyPackages."x86_64-linux"; [
-          nodejs
-          nodePackages.pnpm
+      packages = with nixpkgs.legacyPackages."x86_64-linux"; [
+        nodejs
+        nodePackages.pnpm
 
-          nixd
-        ];
-      };
+        nixd
+      ];
+    };
   };
 }
