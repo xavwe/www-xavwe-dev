92dbf06d1a048e0e126ae288c2fb083cf637d4bc
Xaver Wenhart <git@xavwe.dev>
2025-08-21 08:55:47 +0200

    feat: add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.xavwe.dev",
+
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
   },
+
+  integrations: [sitemap()],
 });
