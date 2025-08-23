9e447b4011b6a62eba8b6ef1223f1a19e3c31db5
Xaver Wenhart <git@xavwe.dev>
2025-08-24 00:27:06 +0200

    chore(flake.nix): add treesitter grammars to dev shell

--- flake.nix
+++ flake.nix
@@ -3,15 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    tree-sitter-astro = {
+      url = "github:virchau13/tree-sitter-astro";
+      flake = false;
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    formatter."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
-    devShells."x86_64-linux".default = nixpkgs.legacyPackages."x86_64-linux".mkShell {
-      packages = with nixpkgs.legacyPackages."x86_64-linux"; [
+    tree-sitter-astro,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    tree-sitter-astro-grammar = pkgs.tree-sitter.buildGrammar {
+      language = "astro";
+      version = "0.0.1";
+      src = tree-sitter-astro;
+    };
+  in {
+    formatter."x86_64-linux" = pkgs.alejandra;
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
         nodejs
         nodePackages.pnpm
 
@@ -19,6 +31,12 @@
 
         nixd
         tree-sitter-grammars.tree-sitter-nix
+
+
+        tree-sitter-astro-grammar
+        tree-sitter-grammars.tree-sitter-css
+        tree-sitter-grammars.tree-sitter-html
+        tree-sitter-grammars.tree-sitter-typescript
       ];
     };
   };
